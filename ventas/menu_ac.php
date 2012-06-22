df57ada4dc55aa8d6ff5c85c2eb67386ffc65c39
guatecham <guatecham@gmail.com>
2012-06-22 10:40:09 -0600

    Minor change
    A minor change that puts the search text box with a new format

--- ventas/menu_ac.php
+++ ventas/menu_ac.php
@@ -1,9 +1,9 @@
 <p>&nbsp;</p>
 <form autocomplete="off" action="add_producto.php" id="frm_autocomplete" name="frm_autocomplete">
 <p align="center">
-	<input type="text" size="3" name="cantidad" id="cantidad" value="1"/>
-	<input type="text" size="30" name="producto" id="producto" onkeypress="validar(event)" onclick="this.value=''" />
+	<input class="searchProduct" type="text" size="2" name="cantidad" id="cantidad" value="1" align="center" />
+	<input class="searchProduct" type="text" size="30" name="producto" id="producto" onkeypress="validar(event)" onclick="this.value=''" />
 	<input type="hidden" id="mesa" name="mesa" value="<?php echo $mesa ?>" />
-	<input type="button" value="+" onclick="validarProducto()" />
+<!-	<input type="button" value="+" onclick="validarProducto()" /> 
 </p>
 </form>
